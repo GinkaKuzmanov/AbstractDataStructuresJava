eea80b6f56fed7dec082fba6b1ac836ffddedf0b
GinkaKuzmanov <ginkakuzmanova@gmail.com>
2020-05-13 22:11:26 +0300

    Implementation of BinarySearchTree, Heap and its respective tests.

--- Data-Structures-Heaps-BST-Exercises/src/main/java/interfaces/Heap.java
+++ Data-Structures-Heaps-BST-Exercises/src/main/java/interfaces/Heap.java
@@ -2,8 +2,12 @@ package interfaces;
 
 public interface Heap<E extends Comparable<E> & Decrease<E>> {
     int size();
+
     void add(E element);
+
     E peek();
+
     E poll();
+
     void decrease(E element);
 }
